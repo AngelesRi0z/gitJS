30e5d5b96d9d80352628714741db292b54255d2e
Angeles Ri0z <rios.zendejas@gmail.com>
2022-01-16 00:15:34 -0600

    version final

--- flujodecontrol.js
+++ flujodecontrol.js
@@ -1,74 +1,34 @@
-/*
 //PARTE 1
 function businessHours(dayNumber,hourNumber) {
-var dayNumber=parseInt(prompt("Ingrese el dia  utilizando número "))
-var hourNumber=parseInt(prompt("Ingrese la hora en horario de 24 hrs"))
+  //var dayNumber=parseInt(prompt("Ingrese el dia con numero siendo 0(lunes) y 6 (domingo) "))
+  var hourNumber=parseInt(prompt("Ingrese la hora en horario de 24 hrs"))
+ 
+  if (hourNumber >= 9 && hourNumber <= 18 && dayNumber >= 0 && dayNumber <= 6){
+      window.alert(true);
+  }else  {
+     
+      window.alert(false)
+  }  
+  }
+  //businessHours();
 
-if (hourNumber >= 9 && hourNumber <= 18 && dayNumber >= 1 && dayNumber <= 5){
-    window.alert(true);
-}else  {
-    
-    window.alert(false)
-}   
+//PARTE 2
+
+function getDayNumber(janFirstDayNumber, yearDayNumber){
+  var res = yearDayNumber % 7;
+  var final = (janFirstDayNumber + res)%7;
+  return final;
 }
-businessHours();
-*/
+//getDayNumber();
 
+//alert("De acuerdo a los datos ingresados el dia cayo en : " + getDayNumber(parseInt(prompt("En que dia inició el año siendo 0(lunes) y 6(domingo))")),parseInt(prompt("Elige un numero del 0 al 365"))));
 
-//Parte 2
+//PARTE 3
 
-var getDayNumber = parseInt(prompt("Dame un día del 1 al 365"));
-var calculator = getDayNumber % 7
-switch (calculator){
-    case 1: 
-        window.alert("Lunes")
-        break;
-    case 2:
-        window.alert("Martes")
-        break;
-    case 3:
-        window.alert("Miercoles")
-        break;
-    case 4:
-        window.alert("Jueves")
-        break;
-    case 5:
-        window.alert("Viernes")
-        break;
-    case 6:
-        window.alert("Sabado")
-        break;
-    case 7:
-        window.alert("Domingo")
-        break;
+function workingHours() {
+  getDayNumber();
+  businessHours();
 }
-
-/*
-//Parte 3
-
-var getDayNumber = parseInt(prompt("Dame un día del 0 al 365"));
-var calculator = getDayNumber % 7
-switch (calculator){
-    case 1: 
-        console.log("Lunes")
-        break;
-    case 2:
-        console.log("Martes")
-        break;
-    case 3:
-        console.log("Miercoles")
-        break;
-    case 4:
-        console.log("Jueves")
-        break;
-    case 5:
-        console.log("Viernes")
-        break;
-    case 6:
-        console.log("Sabado")
-        break;
-    case 7:
-        console.log("Domingo")
-        break;
-}
-*/
+workingHours();
+alert("De acuerdo a los datos ingresados el dia cayo en : " + getDayNumber(parseInt(prompt("En que dia inició el año siendo 0(lunes)")),parseInt(prompt("Elige un numero del 0 al 365"))));
+  
